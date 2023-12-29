dd4628d6c372962d9e3cee677ab53f5397002d8f
z-shaffer <zshaffer@protonmail.com>
2023-12-29 22:46:28 +0100

    Bug with rotation against walls fixed, cutting counter added

---
+++ Frosty Frenzy/Assets/Scripts/CuttingCounter.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CuttingCounter : BaseCounter
+{
+    public override void Interact(Player player)
+    {
+        if (!HasKitchenObject())
+        {
+            if (player.HasKitchenObject())
+            {
+                player.GetKitchenObject().SetKitchenObjectParent(this);
+            }
+            else
+            {
+
+            }
+        }
+        else
+        {
+            if (player.HasKitchenObject())
+            {
+
+            }
+            else
+            {
+                GetKitchenObject().SetKitchenObjectParent(player);
+            }
+        }
+    }
+}
